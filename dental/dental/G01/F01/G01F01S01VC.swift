524308ec05c753cc4e2020bbc4731d09fd2f8cfa
Nixforest <Nixforest21991920@gmail.com>
2018-02-17 12:59:27 +0700

    Update medical record
    Update medical record

--- dental/dental/G01/F01/G01F01S01VC.swift
+++ dental/dental/G01/F01/G01F01S01VC.swift
@@ -120,6 +120,107 @@ class G01F01S01VC: ChildExtViewController {
         return DomainConst.BLANK
     }
     
+    /**
+     * Set data from id
+     * - parameter id:      Id of data
+     * - parameter value:   Value of data
+     */
+    public func setData(id: String, value: String) {
+        for item in self._data.data {
+            if item.id == id {
+                item.name = value
+                break
+            }
+        }
+    }
+    
+    /**
+     * Create input alert
+     * - parameter id:  Id of item
+     */
+    public func createInputAlert(id: String) {
+        var title           = DomainConst.BLANK
+        var message         = DomainConst.BLANK
+        var placeHolder     = DomainConst.BLANK
+        var keyboardType    = UIKeyboardType.default
+        var value           = DomainConst.BLANK
+        switch id {
+        case DomainConst.ITEM_RECORD_NUMBER:
+            title = DomainConst.CONTENT00549
+            message = DomainConst.CONTENT00550
+            value = getData(id: id)
+        default:
+            break
+        }
+        var tbxValue: UITextField?
+        
+        // Create alert
+        let alert = UIAlertController(title: title,
+                                      message: message,
+                                      preferredStyle: .alert)
+        // Add textfield
+        alert.addTextField(configurationHandler: { textField -> Void in
+            tbxValue = textField
+            tbxValue?.placeholder       = placeHolder
+            tbxValue?.clearButtonMode   = .whileEditing
+            tbxValue?.returnKeyType     = .done
+            tbxValue?.keyboardType      = keyboardType
+            tbxValue?.text              = value
+            tbxValue?.textAlignment     = .center
+        })
+        
+        // Add cancel action
+        let cancel = UIAlertAction(title: DomainConst.CONTENT00202, style: .cancel, handler: nil)
+        
+        // Add ok action
+        let ok = UIAlertAction(title: DomainConst.CONTENT00008, style: .default) {
+            action -> Void in
+            if let newValue = tbxValue?.text, !newValue.isEmpty {
+                self.setData(id: id, value: newValue)
+                self.requestUpdate()
+            } else {
+                self.showAlert(message: DomainConst.CONTENT00551,
+                               okHandler: {
+                                alert in
+                                self.createInputAlert(id: id)
+                })
+            }
+        }
+        
+        alert.addAction(cancel)
+        alert.addAction(ok)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    /**
+     * Handle request update data
+     */
+    internal func requestUpdate() {
+        var arrData = [String]()
+        for item in self._data.data {
+            if item.id == DomainConst.ITEM_MEDICAL_HISTORY {
+                for child in item.data {
+                    arrData.append(child.id)
+                }
+            }            
+        }
+        let medicalHistory = String.init(format: "%@%@%@", "[", arrData.joined(separator: ","), "]")
+        MedicalRecordUpdateRequest.request(
+            action: #selector(finishUpdate(_:)),
+            view: self,
+            id: self._id,
+            recordNumber: getData(id: DomainConst.ITEM_RECORD_NUMBER),
+            medicalHistory: medicalHistory)
+    }
+    
+    internal func finishUpdate(_ notification: Notification) {
+        let data = notification.object as! String
+        let model = BaseRespModel(jsonString: data)
+        if model.isSuccess() {
+            self._tblInfo.reloadData()
+        }
+    }
+    
     // MARK: Layout
     
     // MARK: Information table view
@@ -170,6 +271,20 @@ extension G01F01S01VC: UITableViewDataSource {
             cell.imageView?.image = ImageManager.getImage(named: DomainConst.INFORMATION_IMG_NAME, margin: GlobalConst.MARGIN_CELL_X)
             cell.imageView?.contentMode = .scaleAspectFit
             return cell
+        case DomainConst.ITEM_RECORD_NUMBER:
+            let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "Cell")
+            var value = data.name            
+            if value.isEmpty {
+                value = "Bổ sung số bệnh án"
+            }
+            cell.textLabel?.text = value
+            cell.textLabel?.textColor = UIColor.red
+            cell.textLabel?.font = GlobalConst.BASE_FONT
+            cell.textLabel?.lineBreakMode = .byWordWrapping
+            cell.textLabel?.numberOfLines = 0
+            cell.imageView?.image = ImageManager.getImage(named: DomainConst.INFORMATION_IMG_NAME, margin: GlobalConst.MARGIN_CELL_X)
+            cell.imageView?.contentMode = .scaleAspectFit
+            return cell
         default:
             let cell = UITableViewCell(style: .subtitle, reuseIdentifier: "Cell")
             cell.textLabel?.text = data.name
@@ -205,6 +320,9 @@ extension G01F01S01VC: UITableViewDelegate {
                                                                     animated: true)
             }
             break
+        case DomainConst.ITEM_RECORD_NUMBER:
+            createInputAlert(id: data.id)
+            break
         default: break
         }
     }
@@ -217,6 +335,9 @@ extension G01F01S01VC: UITableViewDelegate {
 //            return GlobalConst.LABEL_H * 3
 //        } else 
         if (self._data.data[indexPath.row].name.isEmpty) {
+            if self._data.data[indexPath.row].id == DomainConst.ITEM_RECORD_NUMBER {
+                return UITableViewAutomaticDimension
+            }
             return 0
         } else {
             return UITableViewAutomaticDimension
